82d6e8904bc68dc9cb396014a899fa130ad9bb00
comp-master-byte <demoniac781@gmail.com>
2022-06-09 16:20:09 +0500

    Добавлен config и изменен index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require("express")
+const config = require("config")
 const userRouter = require("./routes/user.routes")
 const taskRouter = require("./routes/task.routes")
 
-const PORT = 8080
+const PORT = config.get("serverPort")
 
 const app = express()
-
 
 app.use(express.json())
 app.use("/api", userRouter)
 app.use("/api", taskRouter)
 
-app.listen(PORT, () => console.log(`Server has been started on PORT: ${PORT}`))
+const serverStarted = () => {
+    try {
+        app.listen(PORT, () => console.log(`Server has been started on PORT: ${PORT}`))
+    } catch(error) {
+        throw new Error(err.message);
+    }
+}
+
+serverStarted()
