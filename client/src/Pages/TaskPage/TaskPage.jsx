3e9ccb2da33b6910136d9b7264e27a71e0119f22
comp-master-byte <demoniac781@gmail.com>
2022-06-10 16:31:45 +0500

    Верстка карточки

--- client/src/Pages/TaskPage/TaskPage.jsx
+++ client/src/Pages/TaskPage/TaskPage.jsx
@@ -3,6 +3,16 @@ import styles from "./TaskPage.module.scss"
 
 export const TaskPage = () => {
     return (
-        <div>TaskPage</div>
+        <div className={styles.taskpage}>
+
+            <div className={styles.card}>
+                <div className={styles.cardInner}>
+                    <div className={styles.cardInner__title}>Заголовок задачи</div>
+                    <div className={styles.cardInner__text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, commodi.</div>
+                    <div className={styles.cardInner__date}>10.06.2022</div>
+                </div>
+            </div>
+
+        </div>
     )
 }
