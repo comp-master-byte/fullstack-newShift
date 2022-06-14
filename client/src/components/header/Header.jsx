fd33dfefac3c8fe5ed5c2ecaf29e398d3c947f87
comp-master-byte <demoniac781@gmail.com>
2022-06-14 08:19:40 +0500

    Улучшено usebility 3

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
 import styles from "./Header.module.scss";
-import { ImTrello } from "react-icons/im"
+import { ImTrello } from "react-icons/im";
+import { GrReactjs } from "react-icons/gr";
+import { AlertComponent } from "../Alert/AlertComponent.jsx"
 
 export const Header = () => {
+
+    const [showPrompt, setShowPrompt] = useState(false);
+
+    const togglePrompt = () => setShowPrompt(prev => !prev);
+
+    setTimeout(() => setShowPrompt(false), 5000);
+
     return (
-        <div className={styles.header}>
-            <div className={styles.headerRow}>
-                <div className={styles.headerRow__logo}>
-                    <ImTrello size="20" />
-                </div>
-                <div className={styles.headerRow__title}>
-                    Task Desk
+        <Fragment>
+            <div className={showPrompt ? styles.headerAlertShow : styles.headerAlertHide}>
+                <AlertComponent alertText="Нажмите на задачу и сможете отредактировать её!" />
+            </div>
+            <div className={styles.header}>
+                <div className={styles.headerRow}>
+                    <div className={styles.headerTitle}>
+                        <div className={styles.headerTitle__logo}>
+                            <ImTrello size="20" />
+                        </div>
+                        <div className={styles.headerTitle__title}>
+                            Task Desk
+                        </div>
+                    </div>
+                    <div onClick={togglePrompt} className={styles.headerPrompt}>
+                        <GrReactjs size="20" />
+                        <div className={styles.headerPrompt__title}>
+                            Подсказка
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Fragment>
     )
 }
